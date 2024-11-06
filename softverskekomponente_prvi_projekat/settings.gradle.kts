01b99db999fcbb9df7019262652bbd6738821bfb
markostojicic03 <mstojicic12923rn@raf.rs>
2024-11-06 13:31:18 +0100

    Dodavanje modula

---
+++ softverskekomponente_prvi_projekat/settings.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "softverskekomponente_prvi_projekat"
+include("specification")
+include("pdfImplementation")
+include("excelImplementation")
+include("csvImplementation")
+include("textImplementation")
